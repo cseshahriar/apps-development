986df272f431acfd47ab9cebbbd19bc051076553
cseshahriar <cse.shahriar.hosen@gmail.com>
2025-08-29 17:23:16 +0600

    stop watch

--- Stopwatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ Stopwatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,20 +1,91 @@
 package com.example.stopwatch
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.SystemClock
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    // input field
+    private lateinit var tvTimer: TextView
+    private lateinit var btnStartPause: Button
+    private  lateinit var btnReset: Button
+
+    // local var
+    private var isRunning = false
+    private var startTime: Long = 0
+    private var pauseTime: Long = 0
+
+    // event handler
+    private val handle = Handler()
+
+    private val stopwatchRunnable = object : Runnable {
+        override fun run() {
+            val elapsedTime = SystemClock.elapsedRealtime() - startTime
+            val minutes = (elapsedTime / 60000).toInt()
+            val seconds = ((elapsedTime % 60000) / 1000).toInt()
+            val milliseconds = ((elapsedTime % 1000) / 10).toInt()
+
+            val formattedTime = if(minutes > 0) {
+                String.format("%02d:%02d:%02d", minutes, seconds, milliseconds)
+            } else {
+                String.format("%02d:%02d", seconds, milliseconds)
+            }
+            tvTimer.text = formattedTime
+            handle.postDelayed(this, 50)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        tvTimer = findViewById(R.id.tvTimer)
+        btnStartPause = findViewById(R.id.btnStartPause)
+        btnReset = findViewById(R.id.btnReset)
+
+
+        btnStartPause.setOnClickListener {
+            if(isRunning) {
+                pausStopWatch()
+            } else {
+                startStopWatch()
+            }
         }
+
+        btnReset.setOnClickListener {
+            resetStopWatch()
+        }
+    }
+
+    fun startStopWatch() {
+        if(!isRunning) {
+            startTime = SystemClock.elapsedRealtime() - pauseTime
+            handle.post(stopwatchRunnable)
+            isRunning = true
+            btnStartPause.text = "Pause"
+        }
+    }
+    fun pausStopWatch() {
+        if(isRunning) {
+            handle.removeCallbacks(stopwatchRunnable)
+            pauseTime = SystemClock.elapsedRealtime() - startTime
+            isRunning = false
+            btnStartPause.text = "Start"
+        }
+    }
+
+    fun resetStopWatch() {
+        handle.removeCallbacks(stopwatchRunnable)
+        isRunning = false
+        pauseTime - 0
+        tvTimer.text = "00.00"
+        btnStartPause.text = "Start"
     }
 }
